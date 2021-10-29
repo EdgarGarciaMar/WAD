2c3f4f72005c3a54307f846950d5b3e4d49dfc7e
EdgarGarciaMar <edgargarcia092010@gmail.com>
2021-10-29 09:41:02 -0500

    ProyectoBase3CM13V4
    Práctica 4

---
+++ proyectoBase3CM13V4/src/main/java/com/ipn/mx/utilerias/hibernateUtil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ipn.mx.utilerias;
+
+import org.hibernate.boot.registry.StandardServiceRegistry;
+
+/**
+ *
+ * @author edgargarcia
+ */
+public class hibernateUtil {
+    private static StandardServiceRegistry registry;
+
+}
